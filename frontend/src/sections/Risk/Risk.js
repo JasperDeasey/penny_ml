379fab936a460a15488c470ab075bafeed81b142
JasperDeasey <60531248+JasperDeasey@users.noreply.github.com>
2024-01-01 16:42:08 -0800

    slight changes to UI

--- frontend/src/sections/Risk/Risk.js
+++ frontend/src/sections/Risk/Risk.js
@@ -29,8 +29,8 @@ const Risk = () => {
             </p>
             <p>
               The liquidity risk is minimized due to the 5-day maximum holding period of each trade.
-              Instead of waiting for a perfect selling opportunity to arise, we sell after 10 days if the 10% rise did not occur.
-              This greatly maximizes liquidity.
+              Instead of waiting for a perfect selling opportunity to arise, we sell after 5 days if the 10% rise did not occur.
+              This greatly increases liquidity.
             </p>
             <p>
               As a result, the portfolio typically has a large portion dedicated to cash.
@@ -39,7 +39,7 @@ const Risk = () => {
             <p>
               As these companies experience extreme price volatility, putting too much weight into any single company subjects the portfolio to risk.
               This risk is mitigated with the formula that calculates the ideal number of shares, through two mechanisms:
-              <li><b>Correlation Reduction Factor:</b> There is a preference for minizing the correlation between assets in the portfolio. If a significant portion of the portfolio is stock A, then there will be low incentive to add more of stock A, as it is correlated to the rest of the portfolio.</li>
+              <li><b>Correlation Reduction Factor:</b> There is a preference for minimizing the correlation between assets in the portfolio. If a significant portion of the portfolio is stock A, then there will be low incentive to add more of stock A, as it is correlated to the rest of the portfolio.</li>
               <li><b>Single Security Weight Reduction Factur:</b> There is a preference for buying less of a security, the more of it that is owned. In these simulations, no more of a security would be bought if it were already 5% of the portfolio. It increases exponentially the less is owned.</li>
             </p>
           </div>
